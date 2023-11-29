55ef39da46c40b8a6ec94544574db8733fbb534a
julowe <10883713+julowe@users.noreply.github.com>
2023-11-28 17:52:01 -0800

    add GAME layer
    changes right side to have no mod/funny/qmk keys, left side is passthrough except for layer toggle key

--- keyboards/gboards/gergo/keymaps/jkl/keymap.c
+++ keyboards/gboards/gergo/keymaps/jkl/keymap.c
@@ -14,6 +14,7 @@ char wpm_str[10];
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define NUMB 2 // numbers/motion
+#define GAME1 3 // Game layer
 
 // Blank template at the bottom
 enum customKeycodes {
@@ -102,11 +103,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------'                     `--------------'
  */
 [NUMB] = LAYOUT_gergo(
-    KC_TRNS, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+    KC_TRNS, KC_1, 	  KC_2,    KC_3,    KC_4,    KC_5,                                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TG(GAME1),
     KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX,                         KC_F6,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU,
     KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  XXXXXXX,  KC_TRNS,     KC_TRNS, KC_F12,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MPLY, KC_MNXT,
                                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_BTN1,  KC_BTN2, KC_TRNS, KC_TRNS
     ),
+
+/* Keymap 3: Game Layer (because Windows Fallout: New Vegas was unhappy with default base layer)
+ *
+ * ,-------------------------------------------.                         ,-------------------------------------------.
+ * |        |      |      |      |      |      |                         |      |      |      |      |      |        |
+ * |--------+------+------+------+------+------|------.           .------|------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *                        .------.   .------.                                 .------.   .-----.
+ *                        |      |   |      |                                 |      |   |     |
+ *                        '------'   '------'                                 `------.   '-----'
+ *                                        ,-------.       ,-------.
+ *                                        |       |       |       |
+ *                                 ,------|-------|       |-------|------.
+ *                                 |      |       |       |       |      |
+ *                                 |      |       |       |       |      |
+ *                                 |      |       |       |       |      |
+ *                                 `--------------'       `--------------'
+ */
+/*
+[BASE] = LAYOUT_gergo(
+    LT(NUMB, KC_ESC),       KC_Q,  KC_W,   KC_E,   KC_R, KC_T,                                          KC_Y,    KC_U, KC_I, KC_O,   KC_P,    KC_PIPE,
+    MT(MOD_LCTL, KC_BSPC),  KC_A,  KC_S,   KC_D,   KC_F, KC_G, XXXXXXX,                       KC_BTN2,  KC_H,    KC_J, KC_K, KC_L,   KC_SCLN, KC_QUOT,
+    TD(TD_RSHIFT_CAPS),     KC_Z,  KC_X,   KC_C,   KC_V, KC_B, XXXXXXX, KC_PGUP,     KC_BTN3, KC_BTN1,  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+  MT(MOD_LGUI, KC_DEL), MT(MOD_LALT, KC_ENT), LT(SYMB, KC_SPC), LT(NUMB, KC_ESC),    LT(SYMB, KC_ENT), LT(NUMB, KC_SPC), KC_TAB, KC_BSPC
+    ),
+*/
+[GAME1] = LAYOUT_gergo(
+    KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R, KC_T,                                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(GAME1),
+    KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F, KC_G, XXXXXXX,                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_RSFT, KC_Z,   KC_X,   KC_C,   KC_V, KC_B, XXXXXXX, KC_PGUP,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                             KC_DEL,   KC_ENT,   KC_SPC,  KC_TAB,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    )
+// TODO: decide, LALT or ENT?
+// TODO: update keymap image?
 };
 
 /* Keymap template
